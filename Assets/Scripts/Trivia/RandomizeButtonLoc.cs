0ff60868155da98d6a9590a546feb7a10709cfd2
aashnakulsh <148536308+aashnakulsh@users.noreply.github.com>
2024-10-14 21:32:11 -0400

    Fixed RandomizeButton bug where buttons weren't showing up

--- Assets/Scripts/Trivia/RandomizeButtonLoc.cs
+++ Assets/Scripts/Trivia/RandomizeButtonLoc.cs
@@ -12,13 +12,17 @@ public class RandomizeButtonLoc : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {        
-        private int _idx = Random.Range(0, 2);   // Generate a random index between 0 and 1
+        int _idx = Random.Range(0, 2); // Generate a random index between 0 and 1
 
         // Assign the right answer button to a location
         _rightAnswerButton.transform.position = _buttonLocs[_idx].position;
+        Debug.Log("Right Answer Button Position: " + _rightAnswerButton.transform.position);
+        Debug.Log("_idx:" + _idx);
 
         // Assign the wrong answer button to the other location
         _wrongAnswerButton.transform.position = _buttonLocs[1 - _idx].position;
+        Debug.Log("Wrong Answer Button Position: " + _wrongAnswerButton.transform.position);
+        Debug.Log("1-_idx:" +  (1 - _idx));
     }
 
     // Update is called once per frame
